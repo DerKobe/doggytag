c5a54426180e2f32d53bdef9d6fd3ceb96e771a2
Philip Claren <kobe@mixxt.net>
2013-07-14 18:42:00 +0200

    Adde cleanup to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,18 +14,14 @@ set :application, 'DoggyTag'
 set :repository, 'git@gitlab.sys.mixxt.net:kobe/koeter_tag.git'
 set :scm, :git
 set :branch, 'master'
-#set :scm_verbose, true
 set :default_environment, { PATH: '/home/doggy/.rbenv/shims:$PATH' }
 
 # deploy config
 set :deploy_to, applicationdir
-#set :deploy_via, :export
 
 default_run_options[:pty] = true
 
-
-
-#after 'deploy:restart', 'deploy:cleanup'
+after 'deploy:restart', 'deploy:cleanup'
 
 # Passenger
 namespace :deploy do
